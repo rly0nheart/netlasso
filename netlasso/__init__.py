29e68124cec2b61fbe1da5788322800781114cfb
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-10-25 01:35:55 +0200

    Update __init__.py

--- netlasso/__init__.py
+++ netlasso/__init__.py
@@ -3,6 +3,6 @@ __about__ = "https://about.me/rly0nheart"
 __version__ = "1.0.0"
 __description__ = """
 # Net Lasso
-**Net Lasso** utilises the [Netlas.io API](https://netlas.io/api) to perform advanced searches for internet-connected (IoT) devices based on user-provided search queries."""
+> **Net Lasso** utilises the [Netlas.io API](https://netlas.io/api) to perform advanced searches for internet-connected (IoT) devices based on user-provided search queries."""
 
-__epilog__ = f"> by [{__author__}]({__about__})"
+__epilog__ = f"# by [{__author__}]({__about__})"
