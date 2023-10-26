09a4491d02d4a41ae47c24ccf264672bc80e16a1
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-10-26 23:20:17 +0200

    Update masonry.py

--- netlasso/masonry.py
+++ netlasso/masonry.py
@@ -4,7 +4,7 @@ from typing import Union
 from rich import print
 from rich.tree import Tree
 
-from .coreutils import format_api_data, save_data
+from .coreutils import data_broker, save_data
 
 
 class Masonry:
@@ -35,14 +35,17 @@ class Masonry:
         self.save_json = save_json
         self.save_csv = save_csv
         self.return_raw = return_raw
+
+        self.first_branch_guide_style: str = "bold blue"
+        self.second_branch_guide_style: str = "dim blue"
         self.main_tree = Tree(
             f"Showing [cyan]{self.limit}[/] results for [italic][yellow]{query}[/][/] at "
             f"{current_time.strftime('%a %b %d %Y, %H:%M:%S %p')}",
             guide_style="bold bright_blue",
         )
 
-    @staticmethod
     def add_branch(
+        self,
         target_tree: Tree,
         branch_title: str,
         branch_data: Union[dict, list],
@@ -57,7 +60,9 @@ class Masonry:
         :param additional_text: Additional text to add at the end of the branch.
         :return: A populated branch.
         """
-        branch = target_tree.add(branch_title)
+        branch = target_tree.add(
+            branch_title, guide_style=self.second_branch_guide_style
+        )
         data_types = [dict, list]
         if type(branch_data) in data_types:
             if type(branch_data) is dict:
@@ -67,45 +72,78 @@ class Masonry:
                     branch.add(additional_text, style="italic")
             else:
                 for index, item in enumerate(branch_data, start=1):
-                    if type(item) is dict:
-                        for item_key, item_value in item.items():
-                            branch.add(f"{item_key}: {item_value}")
-                    else:
-                        branch.add(f"{index}. {item}", style="italic")
+                    branch.add(f"{index}. {item}", style="italic")
+
         return branch
 
-    def add_certificate_branch(self, target_tree: Tree, certificate_data) -> Tree:
-        certificate_branch = target_tree.add("Certificate")
+    def certificate_branch(self, result_branch: Tree, certificate_data: dict) -> Tree:
+        """
+        Adds and populates a Certificate branch to the result_branch.
+
+        :param result_branch: Branch to which the certificate_branch will be added.
+        :param certificate_data: A JSON object containing a result's certificate data to populate the branch with.
+        :return: result_branch with a populated certificate_branch added to it.
+        """
+        certificate_branch = result_branch.add("[bold]Certificate[/]")
         self.add_branch(
             target_tree=certificate_branch,
             branch_title=certificate_data.get("serial_number"),
-            branch_data=format_api_data(
+            branch_data=data_broker(
                 api_data=certificate_data,
                 data_file="certificate/summary.json",
             ),
         )
         self.add_branch(
             target_tree=certificate_branch,
-            branch_title="Issuer",
-            branch_data=format_api_data(
+            branch_title="[bold]Issuer[/]",
+            branch_data=data_broker(
                 api_data=certificate_data.get("issuer"),
                 data_file="certificate/issuer.json",
             ),
         )
         self.add_branch(
             target_tree=certificate_branch,
-            branch_title="Validity",
+            branch_title="[bold]Validity[/]",
             branch_data=certificate_data.get("validity"),
         )
 
         if certificate_data.get("names"):
             self.add_branch(
                 target_tree=certificate_branch,
-                branch_title="Names",
+                branch_title="[bold]Names[/]",
                 branch_data=certificate_data.get("names"),
             )
 
-        return certificate_branch
+        return result_branch
+
+    def whois_branch(self, result_branch: Tree, whois_data: dict) -> Tree:
+        """
+        Adds and populates a WHOIS branch to the result_branch.
+
+        :param result_branch: Branch to which the whois_branch will be added.
+        :param whois_data: A JSON object containing a result's WHOIS data to populate the branch with.
+        :return: result_branch with a populated whois_branch added to it.
+        """
+        whois_branch = result_branch.add("[bold]WHOIS[/]")
+        self.add_branch(
+            target_tree=whois_branch,
+            branch_title="[bold]Net[/]",
+            branch_data=data_broker(
+                api_data=whois_data.get("net"),
+                data_file="host/net.json",
+            ),
+            additional_text=whois_data.get("net").get("description"),
+        )
+        self.add_branch(
+            target_tree=whois_branch,
+            branch_title="[bold]ASN[/]",
+            branch_data=data_broker(
+                api_data=whois_data.get("asn"),
+                data_file="host/asn.json",
+            ),
+        )
+
+        return result_branch
 
     def result_branch(self, main_tree: Tree, result_data: dict) -> Tree:
         """
@@ -115,54 +153,35 @@ class Masonry:
         :param result_data: Data to populate the branch with.
         :return: main_tree with the populated result_branch.
         """
-        summary_data = format_api_data(
-            api_data=result_data, data_file="host/summary.json"
-        )
+        summary_data = data_broker(api_data=result_data, data_file="host/summary.json")
 
         # Add a summary branch to the main_tree and populate it with the summary data
-        branch = main_tree.add(result_data.get("isp"))
+        branch = main_tree.add(
+            result_data.get("isp"), guide_style=self.first_branch_guide_style
+        )
         for summary_key, summary_value in summary_data.items():
             branch.add(f"{summary_key}: {summary_value}", style="dim")
 
         # Add a location branch to the main_tree and populate it with location data
         self.add_branch(
             target_tree=branch,
-            branch_title="Location",
-            branch_data=format_api_data(
+            branch_title="[bold]Location[/]",
+            branch_data=data_broker(
                 api_data=result_data.get("geo"), data_file="host/location.json"
             ),
         )
-
-        # Add a WHOIS branch to the main_tree and populate it with WHOIS data (ASN, NET)
-        whois_branch = branch.add("WHOIS")
-        self.add_branch(
-            target_tree=whois_branch,
-            branch_title="Net",
-            branch_data=format_api_data(
-                api_data=result_data.get("whois").get("net"),
-                data_file="host/net.json",
-            ),
-            additional_text=result_data.get("whois").get("net").get("description"),
-        )
-        self.add_branch(
-            target_tree=whois_branch,
-            branch_title="ASN",
-            branch_data=format_api_data(
-                api_data=result_data.get("whois").get("asn"),
-                data_file="host/asn.json",
-            ),
-        )
+        self.whois_branch(result_branch=branch, whois_data=result_data.get("whois"))
 
         if result_data.get("certificate"):
-            self.add_certificate_branch(
-                target_tree=branch, certificate_data=result_data.get("certificate")
+            self.certificate_branch(
+                result_branch=branch, certificate_data=result_data.get("certificate")
             )
 
         # Add a Domains branch to the main_tree and populate it with a list of domains associated with the result
         if result_data.get("domain"):
             self.add_branch(
                 target_tree=branch,
-                branch_title="Domains",
+                branch_title="[bold]Domains[/]",
                 branch_data=result_data.get("domain"),
             )
 
